a8048d79bd2c413328a0f42d5b66acdb1cb1d842
Artyom Voronin <Artyom.Voronin@konicaminolta.cz>
2023-07-24 17:46:04 +0200

    Add images to database; refactor

--- scrapper/scrapper/scrapper/pipelines.py
+++ scrapper/scrapper/scrapper/pipelines.py
@@ -1,10 +1,17 @@
+import io
 import logging
 import os
+from typing import Optional
 
 import psycopg2
 from psycopg2 import OperationalError
+import requests
 
 from .sreality_item import SrealityItem
+
+
+logging.basicConfig(format='%(levelname)s: [%(asctime)s] [%(name)s:%(lineno)d-%(funcName)20s()] %(message)s',
+                    level=logging.ERROR, datefmt='%d/%m/%Y %I:%M:%S')
 
 
 class ScrapperPipeline:
@@ -26,7 +33,11 @@ class ScrapperPipeline:
 
             self.cursor = self.connection.cursor()
 
-            columns = "name text, image_url text"
+            columns = "name text, image_url text, image bytea"
+
+            # Delete table if exists
+            self.cursor.execute(f"DROP TABLE IF EXISTS {self.table_name}")
+            self.connection.commit()
 
             self.cursor.execute(
                 f"CREATE TABLE IF NOT EXISTS {self.table_name} ({columns})"
@@ -34,6 +45,7 @@ class ScrapperPipeline:
             self.connection.commit()
 
             self.cursor.execute(f"DELETE FROM {self.table_name}")
+            self.connection.commit()
 
             self.logger.info(f"Connected to the database. Table {self.table_name} created if not exists.")
         except OperationalError as e:
@@ -46,12 +58,14 @@ class ScrapperPipeline:
 
         name = item.name
         image_url = item.image_url
+        image = self._get_image(image_url)
 
         sql = f"""
-        INSERT INTO {self.table_name} (name, image_url)
-        VALUES (%s, %s)
+        INSERT INTO {self.table_name} (name, image_url, image)
+        VALUES (%s, %s, %s)
         """
-        self.cursor.execute(sql, (name, image_url))
+
+        self.cursor.execute(sql, (name, image_url, psycopg2.Binary(image.read() if image else b'')))
         self.connection.commit()
         return item
 
@@ -60,3 +74,17 @@ class ScrapperPipeline:
         self.logger.info(f"Closing spider scrapped {spider.name}.")
         self.cursor.close()
         self.connection.close()
+
+    def _get_image(self, image_url: str) -> Optional[io.BytesIO]:
+        """Get image from the url."""
+        try:
+            response = requests.get(image_url)
+
+            if response.status_code == 200:
+                image_bytes = io.BytesIO(response.content)
+                return image_bytes
+            else:
+                self.logger.info(f"Failed to download image. Status code: {response.status_code}")
+
+        except Exception as e:
+            self.logger.error(f"Error occurred: {e}")
--- web/server.py
+++ web/server.py
@@ -1,46 +1,77 @@
+import io
 import logging
 import os
 
-from flask import Flask, render_template
+from PIL import Image
 import psycopg2
 
-
-app = Flask(__name__)
-table_name = os.getenv('DB_TABLE_NAME')
-logger = logging.getLogger(__name__)
+from flask import Flask, render_template
 
 
-def get_db_connection():
-    try:
-        return psycopg2.connect(
-                dbname=os.environ.get('POSTGRES_DB'),
-                user=os.environ.get('POSTGRES_USER'),
-                password=os.environ.get('POSTGRES_PASSWORD'),
-                host=os.environ.get('POSTGRES_HOST'),
-                port=os.environ.get('POSTGRES_PORT'),
-                )
-    except Exception as e:
-        logger.error(f'Failed to connect to db: {e}')
-        return None
+logging.basicConfig(format='%(levelname)s: [%(asctime)s] [%(name)s:%(lineno)d-%(funcName)20s()] %(message)s',
+                    level=logging.ERROR, datefmt='%d/%m/%Y %I:%M:%S')
 
 
-@app.route('/')
-def index():
-    logger.info('Request received')
-    connection = get_db_connection()
-    if connection is None:
-       logger.error('Failed to connect to db')
-       return render_template('failed.html')
+class SrealityApp(Flask):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._logger = logging.getLogger(__name__)
+        self.table_name = os.getenv('DB_TABLE_NAME')
+        self._data = None
 
-    cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM {table_name}")
 
-    logger.info(f"Pull data from table {table_name}")
-    data = cursor.fetchall()
+        self.add_url_rule('/', 'index', self.index)
+        self.add_url_rule('/image/<int:image_id>', 'get_image', self.get_image)
 
-    return render_template('index.html', data=data)
+    def get_data(self):
+        db_connection = self.get_db_connection()
+        if db_connection is None:
+            self._logger.error('Failed to connect to db')
+            return render_template('failed.html')
+
+        cursor = db_connection.cursor()
+        cursor.execute(f"SELECT * FROM {self.table_name}")
+        data = cursor.fetchall()
+        self._data = data
+        return data
+
+    def index(self):
+        data = self.get_data()
+        return render_template('index.html', data=data)
+
+    def get_image(self, image_id):
+        if self._data is None:
+            self.get_data()
+        assert self._data is not None
+
+        item = self._data[image_id]
+        image = item[2]
+        return self._load_image(image)
+
+    def _load_image(self, image_bytes):
+        pil_image = Image.open(io.BytesIO(image_bytes))
+        imb_byte_array = io.BytesIO()
+        pil_image.save(imb_byte_array, format='JPEG')
+        imb_byte_array = imb_byte_array.getvalue()
+        return imb_byte_array, 200, {'Content-Type': 'image/jpeg'}
+
+
+    def get_db_connection(self):
+        try:
+            return psycopg2.connect(
+                    dbname=os.environ.get('POSTGRES_DB'),
+                    user=os.environ.get('POSTGRES_USER'),
+                    password=os.environ.get('POSTGRES_PASSWORD'),
+                    host=os.environ.get('POSTGRES_HOST'),
+                    port=os.environ.get('POSTGRES_PORT'),
+                    )
+        except Exception as e:
+            self._logger.error(f'Failed to connect to db: {e}')
+            return None
+
 
 if __name__ == '__main__':
+    app = SrealityApp(__name__)
     app.run(
         host='0.0.0.0',
         port=8080)
